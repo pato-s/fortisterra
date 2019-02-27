9bf587c5a11b54e809fc2190c2afec69deb9d21c
Martín <chetinchog@yahoo.com>
2019-02-26 23:35:52 -0300

    New Project

---
+++ Source/FortisTerra/FortisTerraGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FortisTerraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FORTISTERRA_API AFortisTerraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
